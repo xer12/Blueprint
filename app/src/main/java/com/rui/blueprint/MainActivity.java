9c654958ace1d8752b485d16b4fe6ba737abd870
rchen <15195866500@163.com>
2022-05-15 22:38:02 +0800

    1.添加了密钥 2.添加了代码混淆 3.更改了apk的图标 4.配置了软件的relase版本和apk释放的名字

--- app/src/main/java/com/rui/blueprint/MainActivity.java
+++ app/src/main/java/com/rui/blueprint/MainActivity.java
@@ -13,7 +13,6 @@ import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.MotionEvent;
